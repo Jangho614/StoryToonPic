ff28d67a7f90291d2698cc75e2bdf2f9ffb86135
pybsh <jnhyeon18@gmail.com>
2024-06-19 05:26:42 +0900

    wip

--- app/src/main/java/com/example/storytoonpic/WebsocketListener.java
+++ app/src/main/java/com/example/storytoonpic/WebsocketListener.java
@@ -17,6 +17,7 @@ public class WebsocketListener extends WebSocketListener {
 
     @Override
     public void onMessage(WebSocket webSocket, String text) {
+        String receiveMOD = "";
         System.out.println("Received message: " + text);
         if(text.equals("drawing pictures")) {
             // todo: 그림을 그리는 중
@@ -27,7 +28,23 @@ public class WebsocketListener extends WebSocketListener {
         if(text.equals("writing story")) {
             // todo: 스토리 짜는 중
         }
-        if(text.equals("done! please send 'GET /download_photo' request")) {
+        if(text.equals("---IMAGE START---")) {
+            receiveMOD = "IMAGE";
+        }
+        if(receiveMOD.equals("IMAGE")) {
+            // todo: 이미지받기
+        }
+        if(text.equals("---IMAGE END---")) {
+            receiveMOD = "";
+        }
+        if(text.equals("---STORY START---")) {
+            receiveMOD = "STORY";
+        }
+        if(receiveMOD.equals("STORY")) {
+            // todo: 스토리받기
+        }
+        if(text.equals("---STORY END---")) {
+            receiveMOD = "";
             webSocket.close(1000, "end");
             Context MainActivity = null;
             new Utils().showToast(MainActivity);
